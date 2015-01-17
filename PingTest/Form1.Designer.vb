5fab271c3e7951908fdc558c4367702df3fbeda5
solidhope <hogue.justin@gmail.com>
2015-01-17 12:20:23 -0500

    Layout Changes

--- PingTest/Form1.Designer.vb
+++ PingTest/Form1.Designer.vb
@@ -30,83 +30,250 @@ Partial Class Form1
         Me.Button6 = New System.Windows.Forms.Button()
         Me.Button7 = New System.Windows.Forms.Button()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar3 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar4 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar5 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar6 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar7 = New System.Windows.Forms.ProgressBar()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.ProgressBar8 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar9 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar10 = New System.Windows.Forms.ProgressBar()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.tooltip = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tooltip.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
         Me.Button1.Location = New System.Drawing.Point(24, 27)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(127, 97)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "PC DESKTOP 1"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(24, 130)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button2.Location = New System.Drawing.Point(157, 27)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(127, 97)
         Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "PC DESKTOP 2"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(157, 27)
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button3.Location = New System.Drawing.Point(290, 27)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(127, 97)
         Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "MAC LAPTOP"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(157, 130)
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button4.Location = New System.Drawing.Point(24, 159)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(127, 97)
         Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Button4"
+        Me.Button4.Text = "PC LAPTOP 1"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(290, 27)
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button5.Location = New System.Drawing.Point(157, 159)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(127, 97)
         Me.Button5.TabIndex = 4
-        Me.Button5.Text = "Button5"
+        Me.Button5.Text = "PC LAPTOP 2"
         Me.Button5.UseVisualStyleBackColor = True
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(290, 130)
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button6.Location = New System.Drawing.Point(290, 159)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(127, 97)
         Me.Button6.TabIndex = 5
-        Me.Button6.Text = "Button6"
+        Me.Button6.Text = "PS4"
         Me.Button6.UseVisualStyleBackColor = True
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(114, 277)
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button7.Location = New System.Drawing.Point(64, 612)
         Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(215, 39)
+        Me.Button7.Size = New System.Drawing.Size(143, 39)
         Me.Button7.TabIndex = 6
-        Me.Button7.Text = "Button7"
+        Me.Button7.Text = "GOOD"
         Me.Button7.UseVisualStyleBackColor = True
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(114, 322)
+        Me.ProgressBar1.Location = New System.Drawing.Point(24, 130)
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(215, 23)
+        Me.ProgressBar1.Size = New System.Drawing.Size(127, 23)
         Me.ProgressBar1.TabIndex = 7
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button8.Location = New System.Drawing.Point(157, 423)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(127, 97)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "PRINTER"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(157, 130)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar2.TabIndex = 9
+        '
+        'ProgressBar3
+        '
+        Me.ProgressBar3.Location = New System.Drawing.Point(290, 130)
+        Me.ProgressBar3.Name = "ProgressBar3"
+        Me.ProgressBar3.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar3.TabIndex = 10
+        '
+        'ProgressBar4
+        '
+        Me.ProgressBar4.Location = New System.Drawing.Point(24, 262)
+        Me.ProgressBar4.Name = "ProgressBar4"
+        Me.ProgressBar4.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar4.TabIndex = 11
+        '
+        'ProgressBar5
+        '
+        Me.ProgressBar5.Location = New System.Drawing.Point(157, 262)
+        Me.ProgressBar5.Name = "ProgressBar5"
+        Me.ProgressBar5.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar5.TabIndex = 12
+        '
+        'ProgressBar6
+        '
+        Me.ProgressBar6.Location = New System.Drawing.Point(290, 262)
+        Me.ProgressBar6.Name = "ProgressBar6"
+        Me.ProgressBar6.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar6.TabIndex = 13
+        '
+        'ProgressBar7
+        '
+        Me.ProgressBar7.Location = New System.Drawing.Point(157, 526)
+        Me.ProgressBar7.Name = "ProgressBar7"
+        Me.ProgressBar7.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar7.TabIndex = 14
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button9.Location = New System.Drawing.Point(229, 612)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(143, 39)
+        Me.Button9.TabIndex = 15
+        Me.Button9.Text = "BAD"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'ProgressBar8
+        '
+        Me.ProgressBar8.Location = New System.Drawing.Point(290, 394)
+        Me.ProgressBar8.Name = "ProgressBar8"
+        Me.ProgressBar8.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar8.TabIndex = 21
+        '
+        'ProgressBar9
+        '
+        Me.ProgressBar9.Location = New System.Drawing.Point(157, 394)
+        Me.ProgressBar9.Name = "ProgressBar9"
+        Me.ProgressBar9.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar9.TabIndex = 20
+        '
+        'ProgressBar10
+        '
+        Me.ProgressBar10.Location = New System.Drawing.Point(24, 394)
+        Me.ProgressBar10.Name = "ProgressBar10"
+        Me.ProgressBar10.Size = New System.Drawing.Size(127, 23)
+        Me.ProgressBar10.TabIndex = 19
+        '
+        'Button10
+        '
+        Me.Button10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button10.Location = New System.Drawing.Point(290, 291)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(127, 97)
+        Me.Button10.TabIndex = 18
+        Me.Button10.Text = "TABLET"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button11.Location = New System.Drawing.Point(157, 291)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(127, 97)
+        Me.Button11.TabIndex = 17
+        Me.Button11.Text = "CELLPHONE 2"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button12.Location = New System.Drawing.Point(24, 291)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(127, 97)
+        Me.Button12.TabIndex = 16
+        Me.Button12.Text = "CELLPHONE 1"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'tooltip
+        '
+        Me.tooltip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.tooltip.Location = New System.Drawing.Point(0, 696)
+        Me.tooltip.Name = "tooltip"
+        Me.tooltip.Size = New System.Drawing.Size(450, 22)
+        Me.tooltip.TabIndex = 22
+        Me.tooltip.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(87, 17)
+        Me.ToolStripStatusLabel1.Text = "PLACEHOLDER"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(450, 393)
+        Me.ClientSize = New System.Drawing.Size(450, 718)
+        Me.Controls.Add(Me.tooltip)
+        Me.Controls.Add(Me.ProgressBar8)
+        Me.Controls.Add(Me.ProgressBar9)
+        Me.Controls.Add(Me.ProgressBar10)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.ProgressBar7)
+        Me.Controls.Add(Me.ProgressBar6)
+        Me.Controls.Add(Me.ProgressBar5)
+        Me.Controls.Add(Me.ProgressBar4)
+        Me.Controls.Add(Me.ProgressBar3)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Button8)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.Button6)
@@ -117,7 +284,10 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.tooltip.ResumeLayout(False)
+        Me.tooltip.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
@@ -128,5 +298,21 @@ Partial Class Form1
     Friend WithEvents Button6 As System.Windows.Forms.Button
     Friend WithEvents Button7 As System.Windows.Forms.Button
     Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar3 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar4 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar5 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar6 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar7 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar8 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar9 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressBar10 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents tooltip As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
 
 End Class
--- PingTest/Form1.vb
+++ PingTest/Form1.vb
@@ -1,4 +1,13 @@
 ﻿Public Class Form1
 
 
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+
+    End Sub
+
+    Private Sub ToolStripStatusLabel1_Click(sender As Object, e As EventArgs) Handles ToolStripStatusLabel1.Click
+
+    End Sub
+
+
 End Class
